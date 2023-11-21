a82b87d715da1bc1b8dfff347df8464eae370f60
github_ayns789 <joly.a.idf@gmail.com>
2023-11-21 12:50:45 +0100

    create entity Country

---
+++ src/main/java/com/project/carnet/entities/Country.java
@@ -0,0 +1,24 @@
+package com.project.carnet.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "country")
+public class Country extends AbstractEntityId{
+
+    @Column(name = "libelle", nullable = false)
+    private String countryLibelle;
+
+    protected Country(){
+
+    }
+
+    public String getCountryLibelle() {
+        return countryLibelle;
+    }
+
+    public void setCountryLibelle(String countryLibelle) {
+        this.countryLibelle = countryLibelle;
+    }
+}
