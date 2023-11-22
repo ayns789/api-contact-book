bed81f8ea4f2d522227d3a0a918e972f90ee4b65
github_ayns789 <joly.a.idf@gmail.com>
2023-11-22 11:13:30 +0100

    entity Country > refactor code and add genertor sequence to id

--- src/main/java/com/project/carnet/entities/Country.java
+++ src/main/java/com/project/carnet/entities/Country.java
@@ -1,24 +1,59 @@
 package com.project.carnet.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
 @Entity
 @Table(name = "country")
+@SequenceGenerator(name = "country.seq_country", sequenceName = "seq_country", allocationSize = 1)
 public class Country {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "country.seq_country")
+    @Column(name = "country_id", nullable = false)
+    private long countryId;
+
     @Column(name = "libelle", nullable = false)
-    private String countryLibelle;
+    private String libelle;
 
     protected Country(){
 
     }
 
-    public String getCountryLibelle() {
-        return countryLibelle;
+    public long getCountryId() {
+        return countryId;
     }
 
-    public void setCountryLibelle(String countryLibelle) {
-        this.countryLibelle = countryLibelle;
+    public void setCountryId(long countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "countryId=" + countryId +
+                ", libelle='" + libelle + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Country country)) return false;
+        return getCountryId() == country.getCountryId() && Objects.equals(getLibelle(), country.getLibelle());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCountryId(), getLibelle());
     }
 }
