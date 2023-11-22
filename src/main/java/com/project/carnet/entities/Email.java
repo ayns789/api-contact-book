cbf32c4578873aae330a734d73caeb2994b36d88
github_ayns789 <joly.a.idf@gmail.com>
2023-11-22 11:08:47 +0100

    entity Email > refactor code and add genertor sequence to id

--- src/main/java/com/project/carnet/entities/Email.java
+++ src/main/java/com/project/carnet/entities/Email.java
@@ -1,45 +1,85 @@
 package com.project.carnet.entities;
 
+import com.project.carnet.enums.EmailTypeEnum;
 import jakarta.persistence.*;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "email")
+@SequenceGenerator(name = "email.seq_email", sequenceName = "seq_email", allocationSize = 1)
 public class Email {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "email.seq_phone")
+    @Column(name = "email_id", nullable = false)
+    private long emailId;
+
     @Column(name = "libelle", nullable = false)
-    private String emailLibelle;
+    private String libelle;
 
     @Column(name = "type", nullable = false)
-    private String emailType;
+    private EmailTypeEnum type;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "contact_id")
-    private Contact contact;
+    private Contact contactId;
 
     protected Email(){
 
     }
 
-    public String getEmailLibelle() {
-        return emailLibelle;
+    public long getEmailId() {
+        return emailId;
     }
 
-    public void setEmailLibelle(String emailLibelle) {
-        this.emailLibelle = emailLibelle;
+    public void setEmailId(long emailId) {
+        this.emailId = emailId;
     }
 
-    public String getEmailType() {
-        return emailType;
+    public String getLibelle() {
+        return libelle;
     }
 
-    public void setEmailType(String emailType) {
-        this.emailType = emailType;
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
     }
 
-    public Contact getContact() {
-        return contact;
+    public EmailTypeEnum getType() {
+        return type;
     }
 
-    public void setContact(Contact contact) {
-        this.contact = contact;
+    public void setType(EmailTypeEnum type) {
+        this.type = type;
+    }
+
+    public Contact getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(Contact contactId) {
+        this.contactId = contactId;
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" +
+                "emailId=" + emailId +
+                ", libelle='" + libelle + '\'' +
+                ", type=" + type +
+                ", contactId=" + contactId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Email email)) return false;
+        return getEmailId() == email.getEmailId() && Objects.equals(getLibelle(), email.getLibelle()) && getType() == email.getType() && Objects.equals(getContactId(), email.getContactId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEmailId(), getLibelle(), getType(), getContactId());
     }
 }
