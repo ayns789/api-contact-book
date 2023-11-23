ec60d3f09f1f590d4d5ce66b38849031f1337ad0
github_ayns789 <joly.a.idf@gmail.com>
2023-11-23 15:46:32 +0100

    add Service package and files

--- src/main/java/com/project/carnet/service/ContactService.java
+++ src/main/java/com/project/carnet/service/ContactService.java
@@ -1,6 +1,12 @@
 package com.project.carnet.service;
 
 
+import com.project.carnet.dto.ContactCreateDTO;
 
 public interface ContactService {
+
+    void createContact(ContactCreateDTO dto);
+
+
+
 }
