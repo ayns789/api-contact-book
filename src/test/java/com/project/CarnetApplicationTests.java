04a5f2b401cb0627a031b548b0b2f388cda60feb
github_ayns789 <joly.a.idf@gmail.com>
2023-12-21 14:59:47 +0100

    CarnetApplicationTests > remove method and import junit in this class

--- src/test/java/com/project/CarnetApplicationTests.java
+++ src/test/java/com/project/CarnetApplicationTests.java
@@ -1,13 +1,11 @@
 package com.project;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarnetApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
