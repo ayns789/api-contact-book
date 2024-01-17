ce366cabacb1ad03b0eebc6438eb59573f264310
github_ayns789 <joly.a.idf@gmail.com>
2024-01-17 13:34:16 +0100

    ContactController > add logs in N-points

--- src/main/java/com/project/controllers/ContactController.java
+++ src/main/java/com/project/controllers/ContactController.java
@@ -5,28 +5,25 @@ import com.project.domain.error.ApiError;
 import com.project.service.ContactService;
 import com.project.service.ExcelFileService;
 import com.project.utils.CarnetUtils;
-import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/contact")
 @RequiredArgsConstructor
+@Log4j2
 public class ContactController {
 
     private final ContactService contactService;
@@ -34,48 +31,57 @@ public class ContactController {
 
     @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
     public ContactDTO create(@Valid @RequestBody ContactDTO contactDTO) {
+        log.info("Call add contact");
         return contactService.create(contactDTO);
     }
 
     @GetMapping(path = "/{id}", consumes = "application/json", produces = "application/json")
     public ContactDTO getContact(@PathVariable("id") Long id) {
+        log.info("Call get contact by id : {}", id);
         return contactService.getContact(id);
     }
 
     @GetMapping(path = "/byLastName", consumes = "application/json", produces = "application/json")
     public List<ContactDTO> getContactByLastname(@RequestParam(value = "last_name") String lastName) {
+        log.info("Call get contact by lastname, with lastName : {}", lastName);
         return contactService.getContactByLastname(lastName);
     }
 
     @GetMapping(path = "/byFirstName", consumes = "application/json", produces = "application/json")
     public List<ContactDTO> getContactByFirstname(@RequestParam(value = "first_name") String firstName) {
+        log.info("Call get contact by firstname, with firstName : {}", firstName);
         return contactService.getContactByFirstname(firstName);
     }
 
     @GetMapping(path = "/byPhone", consumes = "application/json", produces = "application/json")
     public List<ContactDTO> getContactByPhone(@RequestParam(value = "phone_number") String phoneNumber) {
+        log.info("Call get contact by phone, with phoneNumber : {}", phoneNumber);
         return contactService.getContactByPhone(phoneNumber);
     }
 
     @PutMapping(path = "/update/{contactId}", consumes = "application/json", produces = "application/json")
     public ContactDTO update(@PathVariable Long contactId, @Valid @RequestBody ContactDTO contactDTO) {
+        log.info("Call update contact by id : {}", contactId);
         return contactService.update(contactId, contactDTO);
     }
 
     @DeleteMapping(path = "/delete/{id}", consumes = "application/json", produces = "application/json")
     public ContactDTO delete(@PathVariable("id") Long id) {
+        log.info("Call delete contact by id : {}", id);
         return contactService.delete(id);
     }
 
     // save on pc in file path
     @GetMapping("/export")
     public void exportFile() throws IOException {
+        log.info("Call export excel file representing contacts");
         excelFileService.exportFile();
     }
 
 
     @PostMapping("/import")
     public void importFile(@RequestParam("file") MultipartFile file) throws IOException {
+        log.info("Call import excel file contacts to save in database");
         excelFileService.importFile(file);
     }
 
